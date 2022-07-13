d37fa347c72098f27ac120b0f733dc49750ada0e
Mikhail <mishajudoist@gmail.com>
2022-07-13 18:11:31 +0900

    first iter

--- lib/batch/batch.go
+++ lib/batch/batch.go
@@ -1,6 +1,7 @@
 package batch
 
 import (
+	"sync"
 	"time"
 )
 
@@ -8,11 +9,40 @@ type user struct {
 	ID int64
 }
 
+type SafeCounter struct {
+	mu sync.Mutex
+	v  []user
+}
+
+func (c *SafeCounter) Inc(u user) {
+	c.mu.Lock()
+	c.v = append(c.v, u)
+	c.mu.Unlock()
+}
+
 func getOne(id int64) user {
 	time.Sleep(time.Millisecond * 100)
 	return user{ID: id}
 }
 
-func getBatch(n int64, pool int64) (res []user) {
-	return nil
+func getBatch(n int64, pool int64) []user {
+
+	var wg sync.WaitGroup
+	res := SafeCounter{}
+
+	sem := make(chan struct{}, pool)
+
+	for i := 0; int64(i) < n; i++ {
+		wg.Add(1)
+		sem <- struct{}{}
+
+		go func(i int64) {
+			u := getOne(i)
+			<-sem
+			res.Inc(u)
+			wg.Done()
+		}(int64(i))
+	}
+	wg.Wait()
+	return res.v
 }
